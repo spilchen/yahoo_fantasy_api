b91008ad5c48b267cfe9d74d382b3968b487569a
Fabio Amendola <fabio.amendola20@gmail.com>
2020-12-24 14:22:55 -0500

    Fix AttributeError in oauth2_logger.py

--- yahoo_fantasy_api/oauth2_logger.py
+++ yahoo_fantasy_api/oauth2_logger.py
@@ -9,5 +9,5 @@ def cleanup():
     # stderr.  We are going to recreate the logger so that we can use a handler
     # that writes to our log file.
     logging.setLoggerClass(logging.Logger)
-    yahoo_oauth.yahoo_oauth.logger = logging.getLogger('mod_yahoo_oauth')
+    yahoo_oauth.logger = logging.getLogger('mod_yahoo_oauth')
     logging.getLogger('mod_yahoo_oauth').setLevel(logging.WARN)
