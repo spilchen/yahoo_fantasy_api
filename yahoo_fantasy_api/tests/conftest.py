ea3d5d37e18e9b046871cc37768b15afd9e476eb
spilchen <spilchen@gmail.com>
2022-12-19 21:54:25 -0400

    Try out autopep8 in PR (#40)
    * Try out autopep8
    
    * autopep8 action fixes (#41)
    
    Co-authored-by: spilchen <spilchen@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: spilchen <spilchen@users.noreply.github.com>

--- yahoo_fantasy_api/tests/conftest.py
+++ yahoo_fantasy_api/tests/conftest.py
@@ -7,7 +7,7 @@ import mock_yhandler
 
 @pytest.fixture()
 def sc():
-    # For testing, we don't call out to Yahoo!  We just use a sample json file.
+    # For testing, we don't call out to Yahoo! We just use a sample json file.
     # For that reason the OAuth2 session context can be None.
     yield None
 
