f1553d624181c8331b588c2f00bb34d18806d784
DMcP89 <davemcpherson@wochstudios.com>
2025-04-10 20:30:41 -0400

    adding old signature back to avoid breaking api

--- yahoo_fantasy_api/game.py
+++ yahoo_fantasy_api/game.py
@@ -44,9 +44,11 @@ class Game:
         lg = league.League(self.sc, league_id, handler=self.yhandler)
         return lg
 
-    def league_ids(self, is_available=False, game_types=None, game_codes=None, seasons=None):
+    def league_ids(self, year=None, is_available=False, game_types=None, game_codes=None, seasons=None):
         """Return the Yahoo! league IDs that the current user played in
 
+        :param year: Optional year, when provide _league_ids_deprecated() will be used to fetch the league ids 
+        :type is_available: int
         :param is_available: Optional flag to filter out leagues that are not available
         :type is_available: bool
         :param game_types: Optional list of game types to filter league IDs returned. Valid values are full|pickem-team|pickem-group|pickem-team-list
@@ -57,9 +59,40 @@ class Game:
         :type seasons: list[str]
         :returns: List of league ids
         """
+        if year is not None:
+            return self._league_ids_deprecated(year=year)
+
         t = objectpath.Tree(self.yhandler.get_leagues_raw(is_available=is_available, game_types=game_types, game_codes=game_codes,seasons=seasons))
         ids = list(t.execute('$..league_key'))
         return ids
+
+    def _league_ids_deprecated(self, year=None):
+        """Return the Yahoo! league IDs that the current user played in
+
+        :param year: Optional year, used to filter league IDs returned.
+        :type year: int
+        :returns: List of league ids
+        """
+        t = objectpath.Tree(self.yhandler.get_teams_raw())
+        jfilter = t.execute('$..(team_key,season,code)')
+        league_applies = False
+        ids = []
+        for row in jfilter:
+            # We'll see two types of rows that come out of objectpath filter.
+            # A row that has the season/code, then all of the leagues that it
+            # applies too.  Check if the subsequent league applies each time we
+            # get the season/code pair.
+            if 'season' in row and 'code' in row:
+                league_applies = row['code'] == self.code
+                if league_applies is True and year is not None:
+                    league_applies = int(row['season']) == int(year)
+            elif league_applies:
+                assert('team_key' in row)
+                ids.append(self._extract_id_from_team_key(row['team_key']))
+        # Return leagues in deterministic order
+        ids.sort()
+        return ids
+
 
     def _extract_id_from_team_key(self, t):
         """Given a team key, extract just the league id from it
